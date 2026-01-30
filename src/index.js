b2910c11370ca05d7eca13eb5c7ef1ddf438cff7
shaswat-hub <shaswatgairola007@gmail.com>
2026-01-30 20:36:24 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
-import App from "@/App";
+import App from "./App";
 import { Toaster } from './components/ui/sonner';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
