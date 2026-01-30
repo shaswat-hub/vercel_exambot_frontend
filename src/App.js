26962cf973d00ed6e3e5813336f38fc53a5e14ac
shaswat-hub <shaswatgairola007@gmail.com>
2026-01-30 20:40:33 +0530

    adding . replace alias @

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "@/App.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "@/pages/HomePage";
-import AdminPage from "@/pages/AdminPage";
+import HomePage from "./pages/HomePage";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
